7e91cbd717bd7293aab41dda2d898e6a63baf1f2
johnseibert19 <75963301+johnseibert19@users.noreply.github.com>
2025-12-16 18:55:48 +0000

    Deploying to gh-pages from @ johnseibert19/johnseibert19.github.io@ece28c648ae4ba32527f0b629e3b3e59417fa259 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,14 +508,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%6D%73%38%31%31%38%35@%63%6F%6D%6D%6F%6E%77%65%61%6C%74%68%75.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-github',
+        title: 'GitHub',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://github.com/johnseibert19", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/john-seibert", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,14 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=3V3x0-0AAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
