a3bcfaf7c34bafb3703e900693cd85550ba3bbf5
johnseibert19 <75963301+johnseibert19@users.noreply.github.com>
2025-12-16 19:39:39 +0000

    Deploying to gh-pages from @ johnseibert19/johnseibert19.github.io@88a5bf4bb6e723993d806d6b3f0006516d60dc19 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,16 +440,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-my-work-i-did-in-summer-2022-has-been-accepted-into-nature-communications",
+          title: 'My work I did in Summer 2022 has been accepted into Nature Communications!...',
+          description: "",
+          section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-my-work-i-did-in-summer-2022-has-been-accepted-into-nature-communications",
-          title: 'My work I did in Summer 2022 has been accepted into Nature Communications!...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
