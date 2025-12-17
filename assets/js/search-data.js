1f0521c42870c9683f2a84ea7382778b6088dff1
johnseibert19 <75963301+johnseibert19@users.noreply.github.com>
2025-12-17 09:07:56 +0000

    Deploying to gh-pages from @ johnseibert19/johnseibert19.github.io@1f7b309ed78c9239b5f0084ab9df61b7413d3d6c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,12 +9,12 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-projects",
-          title: "projects",
+  },{id: "nav-presentations",
+          title: "presentations",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/presentations/";
           },
         },{id: "nav-blog",
           title: "blog",
